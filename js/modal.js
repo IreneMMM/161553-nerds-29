6faa92b52172f7765264189a2b1861730590ebb5
irene <menshovaiv@gmail.com>
2020-11-10 00:15:28 +0300

    Защита Nerds_исправления

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,10 @@
-const buttonFeedback = document.querySelector('.button-feedback');
-const modalPopup = document.querySelector('.modal');
-const buttonClose = modalPopup.querySelector('.close-button');
+const buttonFeedback = document.querySelector(".button-feedback");
+const modalPopup = document.querySelector(".modal");
+const buttonClose = modalPopup.querySelector(".close-button");
 const feedbackForm = modalPopup.querySelector(".feedback-form");
-const userName = modalPopup.querySelector('[name=name]');
-const userEmail = modalPopup.querySelector('[name=email]');
-const userText = modalPopup.querySelector('[name=text]');
+const userName = modalPopup.querySelector("[name=name]");
+const userEmail = modalPopup.querySelector("[name=email]");
+const userText = modalPopup.querySelector("[name=text]");
 
 let isStorageSupport = true;
 let storageName = "";
@@ -13,33 +13,33 @@ let storageEmail = "";
 try {
   storageName = localStorage.getItem("login");
   storageEmail = localStorage.getItem("email");
-} 
+}
 catch (err) {
   isStorageSupport = false;
 }
 
 
-buttonFeedback.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    modalPopup.classList.add('modal-show');
-    
-    if (storageName) {
-      userName.value = storageName;    
-      userEmail.focus();
-  } 
-   if (storageEmail) {
-   userEmail.value = storageEmail;
-   userText.focus();
-} else {    
+buttonFeedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPopup.classList.add("modal-show");
+
+  if (storageName) {
+    userName.value = storageName;
+    userEmail.focus();
+  }
+  if (storageEmail) {
+    userEmail.value = storageEmail;
+    userText.focus();
+  } else {
     userName.focus();
   }
 });
 
-buttonClose.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    modalPopup.classList.remove('modal-show');
-    modalPopup.classList.remove("modal-error");
-  });
+buttonClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPopup.classList.remove("modal-show");
+  modalPopup.classList.remove("modal-error");
+});
 
 feedbackForm.addEventListener("submit", function (evt) {
   if (!userName.value || !userEmail.value || !userText.value) {
